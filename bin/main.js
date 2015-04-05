492e30fd1db40a2a27acda790bb6ccc6cb43d907
gcnew <gcnew@abv.bg>
2015-04-06 01:12:54 +0300

    - initial structure

---
+++ bin/main.js
@@ -0,0 +1,10 @@
+var cm__src__main_js = (function() {
+document.write('Fern');
+return typeof(main) !== 'undefined' ? main : (void 0);
+})();
+
+if (typeof(cm__src__main_js) === 'function') {
+	cm__src__main_js.call(this);
+} else if (cm__src__main_js) {
+	cm__src__main_js.main.call(this);
+}
